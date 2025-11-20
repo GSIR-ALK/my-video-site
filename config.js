a3f5e20a567e68de86cabf54d019118d2b3ff4fc
GSIR-ALK <779809340@qq.com>
2025-11-21 00:03:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,11 @@ const CONFIG = {
     PASSWORD: "779809",
     
     // 视频文件URL（支持本地或在线视频）
-    VIDEO_URL: "https://sfres.zhipianbang.com/202511202311/83b75eed6cdd2d04223428d0f3f4e351/stock/video/file/2023/12/17/a2e301fabe5fb9880ca6db5d5ac2878e.mp4", // 可以是 "https://example.com/your-video.mp4"
+    VIDEO_URL:"微信视频2025-11-20_235826_608.mp4", // 可以是 "https://example.com/your-video.mp4"
     
     // 可选：视频封面图片
     //VIDEO_POSTER: "poster.jpg"
 
 };
+
 
