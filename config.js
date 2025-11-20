3c42d2946538b288fcfcd1a81eb2fede82910827
GSIR-ALK <779809340@qq.com>
2025-11-20 23:45:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,9 @@ const CONFIG = {
     PASSWORD: "123456",
     
     // 视频文件URL（支持本地或在线视频）
-    VIDEO_URL: "video.mp4", // 可以是 "https://example.com/your-video.mp4"
+    VIDEO_URL: "https://flowus.cn/gaosir/share/25e73e9c-03d5-4980-a539-5c579dc65345?code=J3D6MX", // 可以是 "https://example.com/your-video.mp4"
     
     // 可选：视频封面图片
-    VIDEO_POSTER: "poster.jpg"
+    //VIDEO_POSTER: "poster.jpg"
+
 };
