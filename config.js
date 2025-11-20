91a1ca49919b22efe23d467c4c8c9a4380307c0c
GSIR-ALK <779809340@qq.com>
2025-11-20 23:56:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 // 网站配置 - 在这里修改密码和视频链接
 const CONFIG = {
     // 访问密码（修改为您想要的密码）
-    PASSWORD: "123456",
+    PASSWORD: "779809",
     
     // 视频文件URL（支持本地或在线视频）
-    VIDEO_URL: "https://flowus.cn/gaosir/share/25e73e9c-03d5-4980-a539-5c579dc65345?code=J3D6MX", // 可以是 "https://example.com/your-video.mp4"
+    VIDEO_URL: "https://sfres.zhipianbang.com/202511202311/83b75eed6cdd2d04223428d0f3f4e351/stock/video/file/2023/12/17/a2e301fabe5fb9880ca6db5d5ac2878e.mp4", // 可以是 "https://example.com/your-video.mp4"
     
     // 可选：视频封面图片
     //VIDEO_POSTER: "poster.jpg"
 
 };
+
